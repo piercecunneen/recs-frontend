83b14c2119b4f5a6c26d5a5a580d7f77460adc53
piercecunneen <pcunneen@nd.edu>
2016-12-23 23:49:12 -0500

    add search bar UI to navbar

--- src/javascript/views/shared/navbar.js
+++ src/javascript/views/shared/navbar.js
@@ -4,9 +4,10 @@ var React = require('react');
 var Navbar = require('react-bootstrap/lib/Navbar.js');
 var Nav = require('react-bootstrap/lib/Nav.js');
 var NavItem = require('react-bootstrap/lib/NavItem.js');
-var NavDropdown = require('react-bootstrap/lib/NavDropdown.js');
-var MenuItem = require('react-bootstrap/lib/MenuItem.js');
 var Glyphicon = require('react-bootstrap/lib/Glyphicon.js');
+var button = require('react-bootstrap/lib/Button.js');
+var FormGroup = require('react-bootstrap/lib/FormGroup.js');
+var FormControl = require('react-bootstrap/lib/FormControl.js');
 
 var login = require('../../login/login.js');
 
@@ -57,15 +58,15 @@ var NavBar = React.createClass({
         <Navbar.Toggle />
         <Navbar.Collapse>
           <Nav>
-            <NavItem eventKey={1} href="#">Link 1</NavItem>
-            <NavItem eventKey={2} href="#">Link 2</NavItem>
-            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Another action</MenuItem>
-              <MenuItem eventKey={3.3}>Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey={3.3}>Separated link</MenuItem>
-            </NavDropdown>
+            <NavItem eventKey={1} href="/my-recs">My Recs</NavItem>
+            <NavItem eventKey={2} href="/my-requests">My Requests</NavItem>
+            <Navbar.Form pullLeft>
+              <FormGroup>
+                <FormControl style = {{height: 30}} type="text" placeholder="Search for Music" />
+              </FormGroup>
+              {' '}
+              <button style = {{height: 30}} type="submit"> <Glyphicon glyph="search" /></button>
+            </Navbar.Form>
           </Nav>
           <Nav id = "prof-nav" pullRight>
             <NavItem eventKey={1} href="#"> <Glyphicon glyph="bell" />  Notifications</NavItem>
