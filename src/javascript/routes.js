a2b364209891bbd8119eb1671482a15df0a88956
piercecunneen <pcunneen@nd.edu>
2017-01-25 21:28:36 -0500

    change home page to profile for now

--- src/javascript/routes.js
+++ src/javascript/routes.js
@@ -9,7 +9,7 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var createBrowserHistory = require('history/lib/createBrowserHistory.js');
 
-var home = require('./views/home.js');
+// var home = require('./views/home.js');
 var profile = require('./views/profile/profile.js');
 var myRecs = require('./views/myRecs/my-recs.js');
 var myRequests = require('./views/myRequests/my-requests.js');
@@ -19,7 +19,7 @@ var makeRecommendation = require('./views/recommendations/make-recommendation.js
 
 ReactDom.render(
   r(Router, {history: createBrowserHistory()}, [
-    r(Route, {path: '/', component: home}),
+    r(Route, {path: '/', component: profile}),
     r(Route, {path: '/profile', component: profile}),
     r(Route, {path: '/my-recs', component: myRecs}),
     r(Route, {path: '/my-requests', component: myRequests}),
